55610a234339e4f071e2de737fd5719189715402
ailoo2018 <jcfuentes@ailoo.cl>
2026-01-13 17:17:38 -0300

    fixed container lkjlkj

--- server/api/test.js
+++ server/api/test.js
@@ -3,12 +3,13 @@ export default defineEventHandler(async event => {
   const config = useRuntimeConfig()
 
   return {
+    test: "test",
     webPayCommerceCode: config.webPayCommerceCode,
     webPayApiKey: config.webPayApiKey,
     mercadopagoAccessToken: config.mercadopagoAccessToken,
     env: process.env.NODE_ENV || 'development',
     MERCADO_PAGO_ACCESS_TOKEN: process.env.MERCADO_PAGO_ACCESS_TOKEN,
-    WEBPAY_COMMERCE_CODE: process.env.WEBPAY_COMMERCE_CODE,
-    WEBPAY_API_KEY: process.env.WEBPAY_API_KEY,
+    WEBPAY_COMMERCE_CODE: process.env.WEBPAY_COMMERCE_CODE || '-',
+    WEBPAY_API_KEY: process.env.WEBPAY_API_KEY || '-',
   }
 })
