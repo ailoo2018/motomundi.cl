0da997a9f023225322cc287e7681e721c53e5244
ailoo2018 <jcfuentes@ailoo.cl>
2026-01-16 14:33:13 -0300

    fixed container lkjlkj

---
+++ server/api/login/dologin.js
@@ -0,0 +1,30 @@
+import { getDomainId } from "@/server/ailoo-domain"
+
+export default defineEventHandler(async event => {
+  try {
+
+    const config = useRuntimeConfig()
+    const baseUrl = config.public.w3BaseUrl
+
+    let { username, password } = await readBody(event)
+
+
+    return await $fetch(`${baseUrl}/${getDomainId()}/auth/login`, {
+      method: 'POST',
+      body: {
+        username: username,
+        password,
+      },
+    })
+  }catch(error) {
+    const errorData = error.data
+    throw createError({
+      statusCode: error.statusCode || 500,
+      message: error.message || 'Failed to check auth',
+      data: {
+        code: errorData?.code,
+      },
+    })
+  }
+
+})
