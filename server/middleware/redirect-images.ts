768a6ffd80f740703be34bb00fddf1c3f52036de
ailoo2018 <jcfuentes@ailoo.cl>
2026-02-03 22:21:52 -0300

    fixed container lkjlkj

--- server/middleware/redirect-images.ts
+++ server/middleware/redirect-images.ts
@@ -1,9 +1,13 @@
-export default defineEventHandler((event) => {
+export default defineEventHandler(event => {
   const url = getRequestURL(event)
 
   // Check if the path starts with /Content/products/
-  if (url.pathname.startsWith('/Content/products/')) {
+  if (
+    url.pathname.startsWith('/Content/products/') ||
+    url.pathname.startsWith('/Content/images/no-image')
+  ) {
     const cdnBase = 'https://cdn.motomundi.cl'
+    
     return sendRedirect(event, cdnBase + url.pathname, 301)
   }
 
