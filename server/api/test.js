1d0e061795897786b59f4caafa3ae40d12b42249
ailoo2018 <jcfuentes@ailoo.cl>
2026-02-10 10:49:14 -0300

    fixed container lkjlkj

--- server/api/test.js
+++ server/api/test.js
@@ -11,10 +11,21 @@ export default defineEventHandler(async event => {
     MERCADO_PAGO_ACCESS_TOKEN: process.env.MERCADO_PAGO_ACCESS_TOKEN,
     WEBPAY_COMMERCE_CODE: process.env.WEBPAY_COMMERCE_CODE || '-',
     WEBPAY_API_KEY: process.env.WEBPAY_API_KEY || '-',
-    DLOCAL_GO_API_KEY: config.dlocalApiKey || null,
-    NUXT_DLOCAL_GO_API_KEY: config.dlocalApiKey || null,
+
     DLOCAL_URL: config.dlocalApiUrl,
     TEST: config.test || null,
     TEST2: config.test2 || null,
+    AAA: "-------------",
+
+    DLOCAL_GO_API_KEY: process.env.DLOCAL_GO_API_KEY || '-',
+    NUXT_DLOCAL_GO_API_KEY: process.env.NUXT_DLOCAL_GO_API_KEY || '-',
+
+
+    DLOCAL_GO_SECRET_KEY: process.env.DLOCAL_GO_SECRET_KEY || '-',
+    NUXT_DLOCAL_GO_SECRET_KEY: process.env.NUXT_DLOCAL_GO_SECRET_KEY || '-',
+
+    DLOCAL_GO_BASE_URL: process.env.DLOCAL_GO_BASE_URL || '-',
+    NUXT_DLOCAL_GO_BASE_URL: process.env.NUXT_DLOCAL_GO_BASE_URL || '-',
+
   }
 })
