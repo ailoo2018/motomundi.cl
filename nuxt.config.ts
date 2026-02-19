a2e8afb4dbb69aef83b00200746fabc14da8c394
ailoo2018 <jcfuentes@ailoo.cl>
2026-02-19 12:15:02 -0300

    fixed container lkjlkj

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,6 +30,23 @@ export default defineNuxtConfig({
         type: 'image/x-icon',
         href: '/favicon-2.png',
       }],
+      noscript: [
+        {
+          children: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-57TQSHJ"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          body: true, // This ensures it lands in the <body>, not the <head>
+        },
+      ],
+      script: [
+        {
+          children: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-57TQSHJ');`,
+          type: 'text/javascript',
+        },
+      ],
     },
   },
 
