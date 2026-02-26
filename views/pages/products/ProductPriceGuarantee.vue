dd790b921544157278989f5eb99aeb266d89a4a8
ailoo2018 <jcfuentes@ailoo.cl>
2026-02-26 11:36:23 -0300

    fixed container lkjlkj

---
+++ views/pages/products/ProductPriceGuarantee.vue
@@ -0,0 +1,39 @@
+<template>
+  <span class="free-shipping">
+    <svg
+      width="10"
+      height="8"
+      xmlns="http://www.w3.org/2000/svg"
+      class="icon sprite-line-icons"
+    >
+      <use
+        href="/content/images/svg/0b25363450c5afe3b3f9ba7fe4f4173b.svg#i-icon-check"
+        xlink:href="/content/images/svg/0b25363450c5afe3b3f9ba7fe4f4173b.svg#i-icon-check"
+      />
+    </svg>
+    <a
+      href="/terminos-y-condiciones-precio-minimo-garantizado"
+      class="mtc-link"
+      rel="nofollow"
+    >
+      Precio mínimo garantizado
+    </a>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'ProductPriceGuarantee',
+}
+</script>
+
+<style>
+.add-to-favs:hover svg use {
+  stroke: #d6001c;
+}
+
+.add-to-favs.wished svg use {
+  fill: #d6001c;
+  stroke: #d6001c;
+}
+</style>
