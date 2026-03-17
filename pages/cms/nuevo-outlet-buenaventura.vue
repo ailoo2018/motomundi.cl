1492bdc70261a244c9e39786b1a6960b6a302c66
ailoo2018 <jcfuentes@ailoo.cl>
2026-03-17 18:13:29 -0300

    AppLoader claude recommendation

--- pages/cms/nuevo-outlet-buenaventura.vue
+++ pages/cms/nuevo-outlet-buenaventura.vue
@@ -345,7 +345,7 @@ onUnmounted(() => {
         </div>
         <div class="map-section__embed">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3328.5!2d-70.58!3d-33.45!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzPCsDI3JzAwLjAiUyA3MMKwMzQnNDguMCJX!5e0!3m2!1ses!2scl!4v1"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5325.778064395122!2d-70.7035477!3d-33.33203029999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c750af4802db%3A0x198edbaed2ba2e8f!2sArauco%20Premium%20Outlet%20Buenaventura!5e1!3m2!1sen!2scl!4v1773781860403!5m2!1sen!2scl"
             width="100%"
             height="100%"
             style="border:0;"
