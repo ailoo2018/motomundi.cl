deeac70a4a11b5933ad00a7dddcec992755df45c
ailoo2018 <jcfuentes@ailoo.cl>
2026-03-20 19:50:19 -0300

    new yt section

--- views/pages/home/swiper.vue
+++ views/pages/home/swiper.vue
@@ -9,13 +9,15 @@ const props = defineProps({
   },
 })
 
-// const { isMobile, isTablet, isDesktop } = useDevice()
+const { isMobile, isTablet, isDesktop } = useDevice()
 
+/*
 const deviceType = useState('device-type', () => {
   const event = useRequestEvent()
   return event?.context.deviceType || 'desktop'
 })
 const isMobile = computed(() => deviceType.value === 'mobile')
+*/
 
 
 
